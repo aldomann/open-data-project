efa810a348f44bd00dadaafd6f1685d806ba9f1e
Alfredo Hernández <aldomann.designs@gmail.com>
2017-11-12 22:51:58 +0100

    Updated app

--- crimes-hub/app.R
+++ crimes-hub/app.R
@@ -22,11 +22,23 @@ header <- dashboardHeader(
 	title = "Crimes Hub"
 )
 
+cities.list <- c(
+	"Atlanta",
+	"Baltimore",
+	"Chicago",
+	"Detroit",
+	"New York",
+	"Philadelphia",
+	"San Francisco",
+	"Seattle",
+	"Washington"
+)
+
 sidebar <- dashboardSidebar(
 	sidebarMenu(id = "sidebarmenu",
 							menuItem("About this App", tabName = "home",  icon = icon("home")),
-							menuItem("Single City", tabName = "single",  icon = icon("building")),
-							menuItem("Compare Cities", tabName = "compare", icon = icon("globe"), selected = T),
+							menuItem("Single City", tabName = "single",  icon = icon("building"), selected = T),
+							menuItem("Compare Cities", tabName = "compare", icon = icon("globe")),
 
 							conditionalPanel("input.sidebarmenu == 'single'",
 								checkboxGroupInput("categories", "Category",
@@ -39,11 +51,7 @@ sidebar <- dashboardSidebar(
 																	 selected = c("Total", "Property", "Violent", "Other")
 								),
 								selectInput("city.single", "City",
-														choices = c(
-															"Chicago",
-															"Detroit",
-															"San Francisco"
-														),
+														choices = cities.list,
 														selected = "Chicago",
 														multiple = FALSE,
 														selectize = TRUE
@@ -51,22 +59,16 @@ sidebar <- dashboardSidebar(
 							),
 
 							conditionalPanel("input.sidebarmenu == 'compare'",
-								# checkboxInput("norm", "Normalise data",
-								# 							value = FALSE),
 								radioButtons("norm", "Normalisation",
 														 choices = c(
 														 	"None" = "",
-														 	"By mean" = "mean",
+														 	"By crime mean" = "mean",
 														 	"By population" = "pop"
 														 )),
 
 								selectInput("cities", "City",
-														choices = c(
-															"Chicago",
-															"Detroit",
-															"San Francisco"
-															),
-														selected = c("Chicago", "Detroit"),
+														choices = cities.list,
+														selected = c("Chicago", "San Francisco", "Philadelphia", "Atlanta"),
 														multiple = TRUE,
 														selectize = TRUE
 								)
@@ -125,22 +127,22 @@ server <- function(input, output) {
 
 	output$single.plot <- renderDygraph({
 		# Read data
-		single.df <- read.csv(paste0("data/", tolower(input$city.single), ".csv")) %>%
+		single.df <- read.csv(paste0("data/", gsub(" ", "", tolower(input$city.single)), "_final.csv")) %>%
 			mutate(Date = as.Date(as.character(paste(year, month, 01, sep = "-"), "%Y-%m-%d")))
 
 		# Clean data
 		df.prop <- single.df %>%
-			filter(Category == "PROPERTY CRIMES") %>%
+			filter(Category == "PROPERTY") %>%
 			mutate(Property = N) %>%
 			select(Date, Property)
 
 		df.viol <- single.df %>%
-			filter(Category == "VIOLENT CRIMES") %>%
+			filter(Category == "VIOLENT") %>%
 			mutate(Violent = N) %>%
 			select(Date, Violent)
 
 		df.oth <- single.df %>%
-			filter(Category == "QUALITY OF LIFE CRIMES") %>%
+			filter(Category == "OTHER") %>%
 			mutate(Other = N) %>%
 			select(Date, Other)
 
@@ -152,7 +154,12 @@ server <- function(input, output) {
 
 		# Create time series
 		single.ts <- xts(cbind(single.df[,input$categories]), order.by=single.df$Date)
-		dyRangeSelector(dygraph(single.ts, main = paste("Crimes in", input$city.single)))
+		# dyRangeSelector(dygraph(single.ts, main = paste("Evolution of crimes in", input$city.single)))
+		dyRangeSelector(
+			dyOptions(
+				dygraph(single.ts, main = paste("Evolution of crimes in", input$city.single)),
+				strokeWidth = 2)
+		)
 	})
 
 	output$compare.plot <- renderDygraph({
@@ -161,19 +168,20 @@ server <- function(input, output) {
 		cities.df <- data.frame()
 		for (city in input$cities) {
 			cityname <- paste0(city)
-			df <- read.csv(paste0("data/", tolower(city), ".csv"))
+			df <- read.csv(paste0("data/", gsub(" ", "", tolower(city)), "_final.csv"))
 			df <- df %>%
 				mutate(Date = as.Date(as.character(paste(year, month, 01, sep = "-"), "%Y-%m-%d"))) %>%
-				group_by(Date) %>%
+				group_by(Date, population) %>%
 				dplyr::summarise(N = sum(N))
 
 			if ( input$norm == "mean" ) {
+				meanN <- mean(df$N)
 				df <- df %>%
-					mutate(!!cityname := N/mean(N)) %>%
+					mutate(!!cityname := N/meanN) %>%
 					select(Date, !!cityname)
 			} else if (input$norm == "pop" ){
 				df <- df %>%
-					mutate(!!cityname := N/mean(N)) %>%
+					mutate(!!cityname := N/population) %>%
 					select(Date, !!cityname)
 			} else {
 				df <- df %>%
@@ -189,7 +197,11 @@ server <- function(input, output) {
 
 		# Create time series
 		compare.ts <- xts(cbind(cities.df[,input$cities]), order.by=cities.df$Date)
-		dyRangeSelector(dygraph(compare.ts, main = paste("Crimes in ...")))
+		dyRangeSelector(
+			dyOptions(
+				dygraph(compare.ts, main = paste("Evolution of crimes")),
+				strokeWidth = 2)
+		)
 	})
 
 }
