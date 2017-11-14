8e07a9af66dbb80a86f6d8c16d93c3938b150eee
Alfredo Hernández <aldomann.designs@gmail.com>
2017-11-14 03:21:28 +0100

    Newer Detroit dataset

--- city-mining/mining_detroit_old.R
+++ city-mining/mining_detroit_old.R
@@ -6,19 +6,16 @@
 # Libraries ------------------------------------------------
 
 library(tidyverse)
-library(lubridate)
 library(data.table)
 
 
 # Mining Detroit Data ------------------------------------------------
 
-# setwd("city-mining/")
-
 # Download raw data
 detroit.crimes.file = "data/detroit_raw.csv"
 if (!file.exists(detroit.crimes.file)) {
-	# SRC: https://data.detroitmi.gov/Public-Safety/DPD-All-Crime-Incidents-January-1-2009-December-6-/invm-th67
-	download.file("https://data.detroitmi.gov/api/views/invm-th67/rows.csv?accessType=DOWNLOAD", destfile = detroit.crimes.file)
+	# SRC: https://data.detroitmi.gov/Public-Safety/DPD-Reported-Major-Crimes-2011-2014/75yu-k3gj
+	download.file("https://data.detroitmi.gov/api/views/75yu-k3gj/rows.csv", destfile = detroit.crimes.file)
 }
 
 # Read interesting columns and save into a CSV
@@ -31,13 +28,9 @@ if (file.exists(detroit.crimes.file) & !file.exists(detroit.crimes.file.clean)) 
 rm(detroit.crimes.file, detroit.crimes.file.clean)
 
 # Read cleaned data
-detroit.df <- fread(file="data/detroit_clean.csv", sep = ",", header = TRUE, select = c(5,8))
+detroit.df <- fread(file="data/detroit_clean.csv", sep = ",", header = TRUE, select = c(2, 5,6,9,11))
 detroit.df <- detroit.df %>%
 	rename(Primary.Type = CATEGORY)
-
-# Delete invalid rows
-# detroit.df <- na.omit(detroit.df)
-detroit.df <- detroit.df[!apply(is.na(detroit.df) | detroit.df == "", 1, all),]
 
 # Classify crimes in categories
 source("prim_types_functions.R")
@@ -47,22 +40,47 @@ detroit.df <- detroit.df %>%
 	mutate(Category = find_prim_type(Primary.Type))
 
 # Select the most important variables
-detroit.df <- detroit.df[, c(3,2)]
+detroit.df <- detroit.df[, c(2,3,5,6)]
+
+# Functions gathering geolocalization data
+longitude <- function(x){
+	a <- strsplit(x, "\\(", fixed = FALSE, perl = FALSE, useBytes = FALSE)[[1]][2]
+	a <- gsub(")","", a)
+	longitude <- as.numeric(strsplit(a, ",")[[1]][1])
+}
+
+latitude <- function(x){
+	a <- strsplit(x, "\\(", fixed = FALSE, perl = FALSE, useBytes = FALSE)[[1]][2]
+	a <- gsub(")","", a)
+	latitude <- as.numeric(strsplit(a, ",")[[1]][2])
+}
+
+# Extracting longitude and latitude
+detroit.df$longitude <- sapply(detroit.df$LOCATION, function(x) longitude(x) )
+detroit.df$latitude <- sapply(detroit.df$LOCATION, function(x) latitude(x) )
+
+# Selecting the even more important variables
+detroit.df <- detroit.df[,c(1,2,4,5,6)]
 
 # Date formats
-detroit.df$date <- as.Date(mdy_hms(detroit.df$INCIDENTDATE))
+detroit.df$date <- as.Date(detroit.df$INCIDENTDATE , "%m/%d/%Y %H:%M:%S")
 detroit.df$date <- format(detroit.df$date, "%Y-%m-%d")
 
 # Selecting final variables
-detroit.df <- detroit.df[, c(1,3)]
+detroit.df <- detroit.df[, c(3, 4, 5, 6, 2)]
+colnames(detroit.df)[5] <- "hour"
+
+# Delete invalid rows
+detroit.df <- detroit.df[ ! detroit.df$latitude > 1000, ]
+detroit.df <- na.omit(detroit.df)
 
 # Final Summary
-colnames(detroit.df) <- c("Category", "Date")
+colnames(detroit.df) <- c("Category", "Latitude", "Longitude",  "Date", "Hour")
 detroit.df <- detroit.df %>%
 	group_by(Category, year = year(Date), month = month(Date)) %>%
-	summarise(N = n())
+	summarise(N=n())
 
-# detroit Population ------------------------------------------------------
+# Detroit Population ------------------------------------------------------
 
 pop.file = "pop-data/population_detroit.csv"
 detroit.pop <- fread(pop.file, sep = ";", header= TRUE, select = c(1,2))
@@ -71,8 +89,6 @@ detroit.pop$population <- as.numeric(gsub(",", "", detroit.pop$population))
 detroit.df <- merge(detroit.df, detroit.pop)
 detroit.df$population <- detroit.df$population
 
-# Delete incomplete data
-detroit.df <- detroit.df[!(detroit.df$year==2016 & detroit.df$month==12),]
 
 # Create definitive file
 path = "final-data/detroit_final.csv"
